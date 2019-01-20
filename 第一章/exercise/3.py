07364127878b4eb2c0f2a726c22bb0647aab5564
erhuoyan <2949702406@qq.com>
2019-01-20 15:19:47 +0800

    第一章exercise

---
+++ 第一章/exercise/3.py
@@ -0,0 +1,6 @@
+import turtle
+turtle.pensize(3)
+turtle.cicle(20)
+turtle.cicle(40)
+turtle.cicle(80)
+turtle.cicle(160)
---
+++ 第一章/exercise/5.py
@@ -0,0 +1,5 @@
+import random
+str1 = input("请输入你的名字:")
+print("Hello! {}".format(str1))
+guard=old(str1[0]) % 100
+print("你的幸运数字是",random.choice(range(guard)))
